63a9e2bb175c2ff054ea4d812bd19f9f45b21b7c
fabihasohail <msohailfabiha@gmail.com>
2026-08-18 20:04:06 +0500

    logic applying starting !

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+const supabaseUrl = "https://wmflitippmldqpfvxixt.supabase.co";
+const supabaseKey = "sb_publishable_0VvRE72u8ZALMxvFXcuC5w_vAFO97BS";
+
+
+const { createClient } = supabase;
+const client = createClient(supabaseUrl, supabaseKey);
+
 // form-valid
 
 // let form = document.querySelector("#form-valid");
@@ -16,3 +23,19 @@
 
 
 // console.log(new FormData());
+
+
+
+let loginbtn = document.querySelector("#loginbtn");
+let signupbtn = document.querySelector("#signupbtn")
+
+
+
+ loginbtn && loginbtn.addEventListener("click" , () =>{
+    console.log("ok");
+    window.location.href = "./login.html"
+})
+ signupbtn && signupbtn.addEventListener("click" , () =>{
+    console.log("ok");
+    window.location.href = "./index.html"
+})
