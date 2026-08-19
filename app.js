0df2c511084293015ae34d124b200f60258485fb
fabihasohail <msohailfabiha@gmail.com>
2026-08-19 19:07:03 +0500

    pending

--- app.js
+++ app.js
@@ -5,37 +5,83 @@ const supabaseKey = "sb_publishable_0VvRE72u8ZALMxvFXcuC5w_vAFO97BS";
 const { createClient } = supabase;
 const client = createClient(supabaseUrl, supabaseKey);
 
-// form-valid
 
-// let form = document.querySelector("#form-valid");
-// console.log(form);
-
-
-
-// form.addEventListener("submit", (e)=>{
-// e.preventDefault();
-// console.log("okk");
-// let userDta =  new FormData(form)
-// console.log(userDta);
-// let userInfo = Object.fromEntries(userDta);
-// console.log(userInfo);
-// })
-
-
-// console.log(new FormData());
-
-
-
+//buttons:-
 let loginbtn = document.querySelector("#loginbtn");
 let signupbtn = document.querySelector("#signupbtn")
 
 
 
  loginbtn && loginbtn.addEventListener("click" , () =>{
-    console.log("ok");
+    // console.log("ok");
     window.location.href = "./login.html"
 })
  signupbtn && signupbtn.addEventListener("click" , () =>{
-    console.log("ok");
+    // console.log("ok");
     window.location.href = "./index.html"
+})
+
+
+let userInfo;
+let userDta;
+
+// form:-
+
+let form = document.querySelector("#form-valid");
+let inputs = document.querySelectorAll("input");
+
+//loginwork;-
+
+let logAccount = document.querySelector("#btn");
+
+
+
+if(window.location.pathname.endsWith("/index.html")){
+ form && form.addEventListener("submit", async(e)=>{
+e.preventDefault();
+ userDta =  new FormData(form)
+  userInfo = Object.fromEntries(userDta);
+
+
+
+// supabase insert
+const { error } = await client
+  .from('Users-data')
+  .insert({
+     "name" : userInfo.Username,
+    "course" : userInfo.course,
+    "gender" : userInfo.gender,
+   })
+   //sweets alerts:-
+if(userInfo){
+    Swal.fire({
+  title: "SignUp!",
+  icon: "success",
+  draggable: true
+});
+}
+//Auth ;-
+const { data, error:usererror } = await client.auth.signUp({
+  "email": userInfo.emailid,
+  "password": userInfo.password,
+})
+inputs.forEach((input)=>{
+    input.value = "";
+})
+})
+console.log(userInfo);
+}
+else{
+    console.log("okkkk")
+}
+
+
+
+logAccount && logAccount.addEventListener("click",async(e)=>{
+    e.preventDefault();
+    console.log("okkkkk!");
+    const { data, error } = await client.auth.signInWithPassword({
+  "email": userInfo.emailid,
+  "password": userInfo.password,
+})
 })
